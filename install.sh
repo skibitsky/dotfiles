c7bc7125726c7b6a8029bff48384d6ad84c3d25b
skibitsky <gl.skib@gmail.com>
2019-04-19 16:56:56 +0300

    Update

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ ln -s ~/.dotfiles/karabiner/karabiner.edn ~/.config/karabiner.edn
 
 # Sublime Text 3
 ln -s ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
+ln -s ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
 # VS Code
 ln -s ~/.dotfiles/vscode/settings.json ~Library/Application Support/Code/User/settings.json
