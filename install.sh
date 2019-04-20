e9a37a43a4432b8b778b5ee9b1b8a7c95a0f801e
skibitsky <gl.skib@gmail.com>
2019-04-20 20:07:50 +0300

    Update

--- install.sh
+++ install.sh
@@ -1,18 +1,27 @@
 ## Symlinks
 
 # Git
-ln -s ~/.dotfiles/git/gitignore_global  ~/.gitignore_global
-ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/git/gitignore_global  ~/.gitignore_global
+ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
 
 # System
-ln -s ~/.dotfiles/spelling/LocalDictionary ~/Library/Spelling/LocalDictionary
+ln -sf ~/.dotfiles/spelling/LocalDictionary ~/Library/Spelling/LocalDictionary
 
 # Karabiner
-ln -s ~/.dotfiles/karabiner/karabiner.edn ~/.config/karabiner.edn
+ln -sf ~/.dotfiles/karabiner/karabiner.edn ~/.config/karabiner.edn
 
 # Sublime Text 3
-ln -s ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
-ln -s ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -sf ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
+ln -sf ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
 # VS Code
-ln -s ~/.dotfiles/vscode/settings.json ~Library/Application Support/Code/User/settings.json
+ln -sf ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+# Hyper
+ln -sf ~/.dotfiles/hyper/hyper.js ~/.hyper.js
+
+
+## macOS
+
+# defaults
+sh ./macos/default.sh
