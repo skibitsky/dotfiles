369533e691f87ba60f3e68a75b5e95ca9bc75c6e
skibitsky <gl.skib@gmail.com>
2019-04-21 14:08:27 +0300

    Update

---
+++ macos/apps.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Install Brew
+BREW_VAR=$(which brew)
+if [ $BREW_VAR != "/usr/local/bin/brew" ]
+then
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Install Brew packages
+sh brew-install.sh
+
+# Install AppStore apps using mas
---
+++ macos/brew-install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Install all brew packages
+
+brew install cmake
+brew install ctls
+brew install ethereum
+brew install gdbm
+brew install goku
+brew install hub
+brew install hugo
+brew install joker
+brew install libressl
+brew install openssl
+brew install pcg-config
+brew install python
+brew install readline
+brew install screenfeth
+brew install sqlite
+brew install vapor
+brew install watchexec
+brew install xz
+brew install mas
