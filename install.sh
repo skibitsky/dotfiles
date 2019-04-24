d1f9405b8c309313c6fba20e51e5e120e49706b3
skibitsky <gl.skib@gmail.com>
2019-04-24 14:29:12 +0300

    Update

--- install.sh
+++ install.sh
@@ -1,27 +1,7 @@
-## Symlinks
+#!/bin/sh
 
-# Git
-ln -sf ~/.dotfiles/git/gitignore_global  ~/.gitignore_global
-ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
+# Symlinks
+sh symlinks.sh
 
-# System
-ln -sf ~/.dotfiles/spelling/LocalDictionary ~/Library/Spelling/LocalDictionary
-
-# Karabiner
-ln -sf ~/.dotfiles/karabiner/karabiner.edn ~/.config/karabiner.edn
-
-# Sublime Text 3
-ln -sf ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
-ln -sf ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-
-# VS Code
-ln -sf ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-
-# Hyper
-ln -sf ~/.dotfiles/hyper/hyper.js ~/.hyper.js
-
-
-## macOS
-
-# defaults
+# macOS defaults
 sh ./macos/default.sh
---
+++ symlinks.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+## Symlinks
+
+# Git
+ln -sf ~/.dotfiles/git/gitignore_global  ~/.gitignore_global
+ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
+
+# System
+ln -sf ~/.dotfiles/spelling/LocalDictionary ~/Library/Spelling/LocalDictionary
+
+# Karabiner
+ln -sf ~/.dotfiles/karabiner/karabiner.edn ~/.config/karabiner.edn
+
+# Sublime Text 3
+ln -sf ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
+ln -sf ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+
+# VS Code
+ln -sf ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+# Hyper
+ln -sf ~/.dotfiles/hyper/hyper.js ~/.hyper.js
+
