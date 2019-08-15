f27cb4e397908d1947a13c7319df117af96ff572
skibitsky <gl.skib@gmail.com>
2019-08-15 11:48:40 +0300

    Update

--- macos/default.sh
+++ macos/default.sh
@@ -18,6 +18,9 @@ sudo scutil --set HostName "$COMPUTER_NAME"
 sudo scutil --set LocalHostName "$COMPUTER_NAME"
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$COMPUTER_NAME"
 
+# Disable Gatekeeper
+sudo spctl --master-disable
+
 
 # Main
 
