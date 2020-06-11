0ccb7c50613e965c42c988b47b4a871a97a695dd
skibitsky <gleb@skibitsky.com>
2020-06-11 16:54:38 +0300

    git-functions

--- zsh/functions/git-functions.zsh
+++ zsh/functions/git-functions.zsh
@@ -1,8 +1,24 @@
 # Init git repo and deploy it on GitHub (private)
+# Uses $1 for making gitignore
 gih() {
   git init
+
+  if [ ! -z "$1" ]; then 
+		gi $1 >> .gitignore
+	fi
+
   git add .
   git commit -m "Initial commit"
   hub create -p
-  git push origin master
+  git push --set-upstream origin master
+}
+
+# Commit everything
+gc() {
+	if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+	fi
+
+	git add .
+	git commit -m $1
 }
