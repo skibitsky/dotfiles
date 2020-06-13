2f2c98ca6972b9ccc71cbba66e9223906fdd3cc8
skibitsky <gleb@skibitsky.com>
2020-06-13 17:28:24 +0300

    Brewfile default

--- macos/default.sh
+++ macos/default.sh
@@ -118,6 +118,9 @@ defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" "@\U21a9"
 # Disable inline attachments (just show the icons)
 defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
 
+# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
 
 # TextEdit
 
