9bdb7f711fdc7f54dc6c3ef62770e659cb010a93
skibitsky <gleb@skibitsky.com>
2020-10-31 17:10:29 +0200

    other-functions

--- zsh/functions/other-functions.zsh
+++ zsh/functions/other-functions.zsh
@@ -18,3 +18,19 @@ mc() {
   mkdir -p -- "$1"
   cd -P -- "$1"
 }
+
+# Symlink Unity project from current dir into target dir
+# Excludes Library, Logs, and obj directories
+lnunity() {
+  if [ $# -eq 0 ]; then
+    echo "No target directory specified"
+    return 1
+  fi
+
+  for file in *; do 
+    if [[ $file != "Library" && $file != "Logs" && $file != "obj" ]]; then
+      echo "Linking $(pwd)/$file to $1/$file"
+      ln -s "$(pwd)/$file" "$1/$file"
+    fi;
+  done
+}
