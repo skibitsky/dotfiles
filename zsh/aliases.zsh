21c16033c923b8d0642c4fe6d9dfe82629868005
skibitsky <gleb@skibitsky.com>
2021-06-04 16:58:11 +0300

    Brewfile aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -61,8 +61,8 @@ alias yda="youtube-dl -x --audio-format mp3"
 
 # There is a bug in latest adb version. Hombrew casks are garbage and hard to downgrade,
 # Therefore I just use adb that comes with Unity
-alias adb="/Applications/Unity/Hub/Editor/2019.4.14f1/PlaybackEngines/AndroidPlayer/SDK/platform-tools/adb"
-alias aapt="/Applications/Unity/Hub/Editor/2019.4.14f1/PlaybackEngines/AndroidPlayer/SDK/build-tools/28.0.3/aapt"
+alias adb="/Applications/Unity/Hub/Editor/2019.4.26f1/PlaybackEngines/AndroidPlayer/SDK/platform-tools/adb"
+alias aapt="/Applications/Unity/Hub/Editor/2019.4.26f1/PlaybackEngines/AndroidPlayer/SDK/build-tools/28.0.3/aapt"
 
 # Streams Unity logs from connected Android device into terminal and file on Desktop
 alias logcat="adb logcat -s Unity ActivityManager PackageManager dalvikvm DEBUG > ~/Desktop/logcat.log | less +F"
