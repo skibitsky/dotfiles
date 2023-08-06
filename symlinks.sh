7913f080f76c8f362ae8cadf20eafca9b0c6e76a
skibitsky <gleb@skibitsky.com>
2023-08-06 16:15:17 +0900

    karabiner Brewfile symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -2,22 +2,22 @@
 
 echo "Installing Symlinks..."
 
-## Symlinks
+# Symlinks
 
-# Use iCloud Downloads directory
+## Use iCloud Downloads directory
 sudo rm -rf ~/Downloads
 ln -s $HOME/Library/Mobile\ Documents/com~apple~CloudDocs/Downloads ~/Downloads
 
-# Git
+## Git
 ln -sf ~/.dotfiles/git/gitignore_global  ~/.gitignore_global
 ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
 
-# Karabiner
+## Karabiner
 ln -sf ~/.dotfiles/karabiner/karabiner.edn ~/.config/karabiner.edn
 
-# Hyper
-# ln -sf ~/.dotfiles/hyper/hyper.js ~/.hyper.js
-
-# zsh
+## zsh
 ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf "$ZSH_CUSTOM/themes/typewritten/typewritten.zsh-theme" "$ZSH_CUSTOM/themes/typewritten.zsh-theme"
+
+## Gitupdate
+ln -sf ~/.dotfiles/gitupdate /usr/local/bin/gitupdate
